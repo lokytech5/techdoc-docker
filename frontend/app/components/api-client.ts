a624634d488290e6cd632e0a9f69dfabcc5adb34
lokytech5 <lokytech5@gmail.com>
2024-05-31 14:54:58 +0100

    update next-config

--- frontend/app/components/api-client.ts
+++ frontend/app/components/api-client.ts
@@ -1,17 +1,17 @@
 import axios from "axios";
 import useUserStore from "./useUserStore";
 
-
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8000/api';
 
 const apiClient = axios.create({
-    baseURL: 'http://16.171.173.72:8000/api'
+    baseURL: baseURL
 });
 // baseURL: 'http://13.48.78.236:8000/api'
 // baseURL: 'http://localhost:8000/api'
 
 // Authenticated client
 const authApiClient = axios.create({
-    baseURL: 'http://16.171.173.72:8000/api',
+    baseURL: baseURL,
     headers: {
         'Content-Type': 'application/json'
       }
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['daisyui.com'],
       },
+    env: {
+      NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    }
 }
 
 module.exports = nextConfig
