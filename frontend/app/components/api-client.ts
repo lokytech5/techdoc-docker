b6e9f2d1deed576780398b2078b097325dfed8dc
lokytech5 <lokytech5@gmail.com>
2024-05-31 15:29:52 +0100

    update ip

--- frontend/app/components/api-client.ts
+++ frontend/app/components/api-client.ts
@@ -4,14 +4,14 @@ import useUserStore from "./useUserStore";
 const baseURL = process.env.NEXT_PUBLIC_BASE_URL || 'http://16.171.173.72:8000/api';
 
 const apiClient = axios.create({
-    baseURL: 'http://16.171.173.72:8000/api'
+    baseURL: 'http://16.171.16.29:8000/api'
 });
 // baseURL: 'http://13.48.78.236:8000/api'
 // baseURL: 'http://localhost:8000/api'
 
 // Authenticated client
 const authApiClient = axios.create({
-    baseURL: 'http://16.171.173.72:8000/api',
+    baseURL: 'http://16.171.16.29:8000/api',
     headers: {
         'Content-Type': 'application/json'
       }
