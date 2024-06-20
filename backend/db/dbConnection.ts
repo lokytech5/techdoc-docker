5cff16f201eb66fcf29d5f5f6aa08347a85e5e1a
lokytech5 <lokytech5@gmail.com>
2024-06-20 18:58:44 +0100

    update prisma db

--- backend/db/dbConnection.ts
+++ backend/db/dbConnection.ts
@@ -9,6 +9,6 @@ export async function dbConnect() {
         console.log("Connected to MongoDB successfully.");
     } catch (error) {
         console.error("Unable to connect to the database:", error);
-        process.exit(1); // Exit the application if we can't connect to the database
+        process.exit(1); // Exit the application if we can't connect to the db
     }
 }
